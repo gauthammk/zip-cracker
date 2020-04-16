5df4d44980d6f6b077677d02ca04804c2936ab20
Gautham MK <47458585+gauthammk@users.noreply.github.com>
2020-04-16 19:21:13 +0530

    Update zip_crack.py

--- zip_crack.py
+++ zip_crack.py
@@ -11,12 +11,12 @@ print "Total passwords to test: " + str(num_words)
 #try the passwords one by one against the zip file
 with open(wordlist, "rb") as wordlist:
 	for word in wordlist:
-	print 'trying password: ' + word
-	try:
-		zip_file.extractall(pwd=word.strip())
-	except Exception:
-		continue
-else:
-	print "[+] Password found: " + word.decode().strip()
-	exit(0)
+		print 'trying password: ' + word
+		try:
+			zip_file.extractall(pwd=word.strip())
+		except Exception:
+			continue
+		else:
+			print "[+] Password found: " + word.decode().strip()
+			exit(0)
 print "[!] Password not found, try other wordlist."
